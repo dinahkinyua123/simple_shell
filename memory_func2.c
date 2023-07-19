f26bac92caa56d1cd2d7f13a543129c849ec54fe
yNOtOmosh <onyango.omondi.tony@gmail.com>
2023-07-19 01:40:02 -0700

    this functions work on memory allocatons and address

---
+++ memory_func2.c
@@ -0,0 +1,19 @@
+#include "Shell.h"
+
+/**
+ * bfree - this frees a pointer and NULLs the address
+ * @ptr: a address of the pointer to free
+ *
+ * Return: 1 if freed, otherwise 0.
+ */
+
+int bfree(void **ptr)
+{
+	if (ptr && *ptr)
+	{
+		free(*ptr);
+		*ptr = NULL;
+		return (1);
+	}
+	return (0);
+}
